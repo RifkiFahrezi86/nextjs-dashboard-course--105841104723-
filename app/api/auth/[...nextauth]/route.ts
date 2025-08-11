881317029f97e7e4c9fde10a54721720c93544ae
Rifki Fahrezi <rifkifahrezi86@example.com>
2025-08-11 18:55:23 +0800

    Remove all files

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,4 +0,0 @@
-// app/api/auth/[...nextauth]/route.ts
-import { GET, POST } from '@/app/lib/auth';
-
-export { GET, POST };
--- next.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    runtime: 'nodejs', // Paksa menggunakan Node.js Runtime
-  },
-};
-
-module.exports = nextConfig;
